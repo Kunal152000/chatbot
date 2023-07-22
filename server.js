e0ed175fc7aa2ce6000642cd2e8e13aa07db8cb0
kunal <kunal15febjha@gmail.com>
2023-07-22 11:56:01 +0530

    edits

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const bodyParser = require("body-parser");
 const chatbot = require("./chatbot");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/whatsapp-webhook", chatbot);
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
